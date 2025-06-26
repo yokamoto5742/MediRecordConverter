ca19ba1a08a630c3cf2b18a186ad51388105ca7a
yokamoto5742-h <yokamoto5742@gmail.com>
2025-06-26 19:32:45 +0900

    アイコンの変更

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.SuspendLayout();
             // 
             // MainForm
@@ -35,9 +36,10 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1082, 756);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
-            this.Text = "JSON形式変換";
+            this.Text = "MediRecordConverter";
             this.ResumeLayout(false);
 
         }
